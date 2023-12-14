77ab8b6acb8f7a8cb8f73e3d17fa2eb06dfc7990
julessilvestri <jules.sil05@gmail.com>
2023-12-14 16:40:38 +0100

    add user messsage

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,5 +1,9 @@
 const socket = io();
 
+// Demandez à l'utilisateur de fournir un nom
+const username = prompt('Entrer un nom :');
+socket.emit('setUsername', username);
+
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
@@ -7,14 +11,22 @@ const messages = document.getElementById('messages');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (input.value) {
-        socket.emit('chat message', input.value);
+        socket.emit('chat message', { message: input.value });
         input.value = '';
     }
 });
 
-socket.on('chat message', (msg) => {
-    const item = document.createElement('li');
-    item.textContent = msg;
+socket.on('chat message', (data) => {
+    const item = document.createElement('ul');
+    const usernameElement = document.createElement('p');
+    const messageElement = document.createElement('li');
+
+    usernameElement.textContent = data.username;
+    messageElement.textContent = data.message;
+
+    item.appendChild(usernameElement);
+    item.appendChild(messageElement);
+
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
--- server.js
+++ server.js
@@ -9,17 +9,27 @@ const io = new Server(server);
 app.use(express.static('app'));
 app.set('trust proxy', true);
 
+const userSockets = {};
+
 io.on('connection', (socket) => {
   let clientIP = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
-
   clientIP = (clientIP === '::1') ? '127.0.0.1' : clientIP;
 
   console.log(`User connected with IP : ${clientIP}`);
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  socket.on('setUsername', (username) => {
+    userSockets[username] = socket;
+    console.log(`User connected: ${username}`);
   });
+  
+  socket.on('chat message', (data) => {
+    const username = Object.keys(userSockets).find((key) => userSockets[key] === socket);
+  
+    if (username) {
+      console.log('message: ' + data.message);
+      io.emit('chat message', { username, message: data.message });
+    }
+  })
 });
 
 const port = process.env.PORT || 3000;
